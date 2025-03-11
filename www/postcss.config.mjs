4dd6f3174c301e237b69c376339da6f381e05b5f
vzsoares <vinicius.zenha@gmail.com>
2025-03-11 19:13:17 -0300

    fix: scripts

--- www/postcss.config.mjs
+++ www/postcss.config.mjs
@@ -2,6 +2,15 @@ export default {
     plugins: {
         /** @type {import('tailwindcss').Config} */
         tailwindcss: { content: ["./*.html", "./*.js"] },
-        autoprefixer: {},
+        autoprefixer: {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        serif: ["Playfair Display", "serif"],
+                        sans: ["Source Sans Pro", "sans-serif"],
+                    },
+                },
+            },
+        },
     },
 };
