4fe8bb6e224a63686370213f2daadbac53af137a
vzsoares <vinicius.zenha@gmail.com>
2025-03-11 18:27:05 -0300

    fix: css

--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./*.html', './*.js'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'serif': ['Playfair Display', 'serif'],
-        'sans': ['Source Sans Pro', 'sans-serif'],
-      },
+    content: ["./*.html", "./*.js"],
+    theme: {
+        extend: {
+            fontFamily: {
+                serif: ["Playfair Display", "serif"],
+                sans: ["Source Sans Pro", "sans-serif"],
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+};
+
